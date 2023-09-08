5ad400181b4bd91a4ca896a5bd116f9aff87d7ea
amandlyyptr <143859474+amandlyyptr@users.noreply.github.com>
2023-09-08 19:23:43 +0700

    Add files via upload

---
+++ coding.py
@@ -0,0 +1,28 @@
+# Latihan tugas konversi suhu
+
+# Progam konversi suhu celcius kedalam farenheit
+
+celcius = float(input('Masukkan suhu kedalam celcius :'))
+print ("Suhu adalah", celcius, "celcius")
+
+# Farenheit
+farenheit = ((9/5)*celcius)+32
+print ("suhu dalam farenheit adalah", farenheit, "farenheit")
+
+# Progam konversi suhu farenheit kedalam celcius
+
+farenheit = float(input('Masukkan suhu kedalam farenheit :'))
+print ("suhu adalah", farenheit, "farenheit")
+
+# celcius
+celcius = ((5/9)*farenheit)-32
+print ("Suhu dalam celcius adalah", celcius, "celcius")
+
+# Progam konversi suhu kelvin kedalam celcius
+
+kelvin = float(input('masukkan suhu kedalam kelvin :'))
+print ("suhu adalah", kelvin, "kelvin")
+
+#celcius
+celcius = kelvin-273
+print ("Suhu dalam celcius adalah", celcius, "celcius")
